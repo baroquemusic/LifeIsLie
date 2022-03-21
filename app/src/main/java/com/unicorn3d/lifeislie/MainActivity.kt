2f47a89a19db9b474fd07e6f82c38425fb9209c6
Gabor Kovacs <gkovacs72@gmail.com>
2022-03-21 06:03:15 -1000

    signed

--- app/src/main/java/com/unicorn3d/lifeislie/MainActivity.kt
+++ app/src/main/java/com/unicorn3d/lifeislie/MainActivity.kt
@@ -2,10 +2,16 @@ package com.unicorn3d.lifeislie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+
+private lateinit var firebaseAnalytics: FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAnalytics = Firebase.analytics
     }
 }
